b175ee179d341950dda4ec8f75e247b33bfb0d5f
tereka114 <m.d.r.y.h0114@gmail.com>
2015-09-13 14:16:14 +0900

    prepare directries

--- src/exec_script/learnFeatureByModel.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-feature_dir = "feature_vector"
-parameter_file = "parameter/LasagneNeuralNetwork.json"
-for feature_file_name in os.listdir(feature_dir):
-	feature_file = os.path.join(feature_dir,feature_file_name)
-	subprocess.call("python boosting_prediction.py %s %s" % (feature_file,parameter_file),shell=True)
